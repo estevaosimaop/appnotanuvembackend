a402a5026c88dd78867ca463a052d11f678127bf
estevaosimaop <estevaosimaop@gmail.com>
2026-03-24 23:28:48 +0000

    mudança no back 1

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
   res.send(`Hello ${name}!`);
 });
 
+const users = [
+  {
+    nome: "User 1",
+    idade:10
+},
+{
+  nome: "User 2",
+  idade:20
+},
+{
+  nome: "User 3",
+  idade:30
+}
+]
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
 const port = parseInt(process.env.PORT) || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
